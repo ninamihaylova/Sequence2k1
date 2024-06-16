dad1ae0547f334a40ece2e8de9a03446c14af633
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 23:20:57 +0300

    Initial commit

---
+++ Sequence2k1/Program.cs
@@ -0,0 +1,14 @@
+﻿ class Sequence2k1 {
+    public static void Main (string[]args)
+    {
+
+        int n = int.Parse(Console.ReadLine());
+        int number=1;
+        while (number<=n) {
+            Console.WriteLine(number);
+            number=number*2+1;
+
+        }
+
+    }
+}
